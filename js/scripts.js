e894a64f7fdf5bc73ca3c3896d2f7ca027087f40
G0blinC0de <brewbrotherred@gmail.com>
2023-12-06 12:53:15 -0800

    fix stuff

--- js/scripts.js
+++ js/scripts.js
@@ -102,46 +102,46 @@ btnHold.addEventListener('click', function () {
 
 // Coin Flip??
 
-currentCoinTotal = [0, 0];
+// currentCoinTotal = [0, 0];
 
-document.addEventListener('DOMContentLoaded', () => {
-    const btnCoin = document.getElementById('btnCoin');
-    const coinReset = document.getElementById('btnCoinReset');
+// document.addEventListener('DOMContentLoaded', () => {
+//     const btnCoin = document.getElementById('btnCoin');
+//     const coinReset = document.getElementById('btnCoinReset');
 
-    btnCoin.addEventListener('click', function () {
-        const coinToss = Math.trunc(Math.random() * 2) + 1;
-        if (coinToss === 1) {
-            flip("heads");
-            currentCoinTotal[0] += 1;
-            document.getElementById("totalHeads").innerText = currentCoinTotal[0];
-        } else {
-            flip("tails");
-            currentCoinTotal[1] += 1;
-            document.getElementById("totalTails").innerText = currentCoinTotal[1];
-        }
-    });
+//     btnCoin.addEventListener('click', function () {
+//         const coinToss = Math.trunc(Math.random() * 2) + 1;
+//         if (coinToss === 1) {
+//             flip("heads");
+//             currentCoinTotal[0] += 1;
+//             document.getElementById("totalHeads").innerText = currentCoinTotal[0];
+//         } else {
+//             flip("tails");
+//             currentCoinTotal[1] += 1;
+//             document.getElementById("totalTails").innerText = currentCoinTotal[1];
+//         }
+//     });
 
-    function flip(coin) {
-        const headsImage = document.getElementById('Heads');
-        const tailsImage = document.getElementById('Tails');
-        const resultText = document.getElementById("result");
-        resultText.textContent = coin; // Update text
+//     function flip(coin) {
+//         const headsImage = document.getElementById('Heads');
+//         const tailsImage = document.getElementById('Tails');
+//         const resultText = document.getElementById("result");
+//         resultText.textContent = coin; // Update text
 
-        if (coin === "heads") {
-            headsImage.classList.remove('hidden');
-            tailsImage.classList.add('hidden');
-        } else {
-            headsImage.classList.add('hidden');
-            tailsImage.classList.remove('hidden');
-        }
-    };
-    coinReset.addEventListener('click', function () {
-        currentCoinTotal = [0, 0]
-        document.getElementById("totalHeads").innerText = currentCoinTotal[0];
-        document.getElementById("totalTails").innerText = currentCoinTotal[1];
-        const headsImage = document.getElementById('Heads');
-        const tailsImage = document.getElementById('Tails');
-        headsImage.classList.add('hidden');
-        tailsImage.classList.add('hidden');
-    });
-});
+//         if (coin === "heads") {
+//             headsImage.classList.remove('hidden');
+//             tailsImage.classList.add('hidden');
+//         } else {
+//             headsImage.classList.add('hidden');
+//             tailsImage.classList.remove('hidden');
+//         }
+//     };
+//     coinReset.addEventListener('click', function () {
+//         currentCoinTotal = [0, 0]
+//         document.getElementById("totalHeads").innerText = currentCoinTotal[0];
+//         document.getElementById("totalTails").innerText = currentCoinTotal[1];
+//         const headsImage = document.getElementById('Heads');
+//         const tailsImage = document.getElementById('Tails');
+//         headsImage.classList.add('hidden');
+//         tailsImage.classList.add('hidden');
+//     });
+// });
