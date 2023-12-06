cb92310775121abc8bb646dc36b6bda604671b1d
G0blinC0de <brewbrotherred@gmail.com>
2023-12-06 10:27:36 -0800

    correct pig dice

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,7 @@
-// function Game(score, gamesWon, turnTotal, activePlayer, playing) {
-//     this.score = score;
-//     this.gamesWon = gamesWon
-//     this.turnTotal = turnTotal;
-//     this.activePlayer = activePlayer;
-//     this.playing = playing;
-// }
-
-function Game(score, turnTotal, activePlayer, playing) {
+function Game(score, turnTotal, activePlayer) {
     this.score = score;
     this.turnTotal = turnTotal;
     this.activePlayer = activePlayer;
-    this.playing = playing;
 }
 
 Game.prototype.turnSwap = function () {
@@ -21,7 +12,6 @@ Game.prototype.turnSwap = function () {
 Game.prototype.rollDice = function () {
     const roll = Math.trunc(Math.random() * 6) + 1;
     if (roll === 1) {
-        this.turnTotal = 0;
         this.turnSwap();
     } else {
         this.turnTotal += roll;
@@ -31,36 +21,64 @@ Game.prototype.rollDice = function () {
 
 Game.prototype.hold = function () {
     this.score[this.activePlayer] += this.turnTotal;
-    this.turnTotal = 0;
     this.gameWinCheck();
     this.turnSwap();
 };
 
 Game.prototype.gameWinCheck = function () {
     if (this.score[this.activePlayer] >= 100) {
-        this.playing = false;
+        winButtonDisable();
     }
-}
+};
 
 // UI logic
-let players = new Game([0, 0], 0, 0, false);
-// Event listeners for roll and hold buttons
-
-// const diceEl = document.querySelector('.dice');
-// const btnNew = document.querySelector('.btn--new');
-// const btnRoll = document.('.btn--roll');
-// const btnHold = document.querySelector('.btn--hold');
+let players = new Game([0, 0], 0, 0);
 
 const diceEl = document.getElementById('diceImage');
 const btnNew = document.getElementById('newGameButton');
 const btnRoll = document.getElementById('rollButton');
 const btnHold = document.getElementById('holdButton');
 
-// btnNew.addEventListener('click', function () {
+// UI function:
+const winButtonDisable = function () {
+    btnRoll.disabled = true;
+    btnHold.disabled = true;
+}
 
-// });
+const scoreUpdate = function () {
+    document.getElementById('current--0').innerText = 0;
+    document.getElementById('current--1').innerText = 0;
+    document.getElementById('total--0').innerText = players.score[0];
+    document.getElementById('total--1').innerText = players.score[1];
+}
 
+const rollUpdate = function () {
+    players.rollDice();
+    let activePlayer = players.activePlayer
+    if (activePlayer === 0) {
+        document.getElementById('current--0').innerText = players.turnTotal;
+    } else {
+        document.getElementById('current--1').innerText = players.turnTotal;
+    }
+}
 
+// Event listeners for new game, roll, and hold buttons
+btnNew.addEventListener('click', function () {
+    btnRoll.disabled = false;
+    btnHold.disabled = false;
+    players.score = [0, 0]
+    scoreUpdate();
+});
+
+btnRoll.addEventListener('click', function () {
+    rollUpdate();
+});
+
+btnHold.addEventListener('click', function () {
+    players.hold();
+    scoreUpdate();
+    rollUpdate();
+});
 
 // Coin Flip??
 
