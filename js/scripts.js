3ce152d47bb60b53070f4c3083f921fd8dd46119
G0blinC0de <brewbrotherred@gmail.com>
2023-12-07 08:59:36 -0800

    add original js

--- js/scripts.js
+++ js/scripts.js
@@ -33,7 +33,7 @@ Game.prototype.gameWinCheck = function () {
 };
 
 // UI logic
-let players = new Game([0, 0], 0, 0, 0);
+// let players = new Game([0, 0], 0, 0, 0);
 
 const diceImg = document.getElementById('diceImage');
 const btnNew = document.getElementById('newGameButton');
@@ -100,48 +100,50 @@ btnHold.addEventListener('click', function () {
     activePlayerUpdate();
 });
 
+
+
 // Coin Flip??
 
-// currentCoinTotal = [0, 0];
+currentCoinTotal = [0, 0];
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const btnCoin = document.getElementById('btnCoin');
-//     const coinReset = document.getElementById('btnCoinReset');
+document.addEventListener('DOMContentLoaded', () => {
+    const btnCoin = document.getElementById('btnCoin');
+    const coinReset = document.getElementById('btnCoinReset');
 
-//     btnCoin.addEventListener('click', function () {
-//         const coinToss = Math.trunc(Math.random() * 2) + 1;
-//         if (coinToss === 1) {
-//             flip("heads");
-//             currentCoinTotal[0] += 1;
-//             document.getElementById("totalHeads").innerText = currentCoinTotal[0];
-//         } else {
-//             flip("tails");
-//             currentCoinTotal[1] += 1;
-//             document.getElementById("totalTails").innerText = currentCoinTotal[1];
-//         }
-//     });
+    btnCoin.addEventListener('click', function () {
+        const coinToss = Math.trunc(Math.random() * 2) + 1;
+        if (coinToss === 1) {
+            flip("heads");
+            currentCoinTotal[0] += 1;
+            document.getElementById("totalHeads").innerText = currentCoinTotal[0];
+        } else {
+            flip("tails");
+            currentCoinTotal[1] += 1;
+            document.getElementById("totalTails").innerText = currentCoinTotal[1];
+        }
+    });
 
-//     function flip(coin) {
-//         const headsImage = document.getElementById('Heads');
-//         const tailsImage = document.getElementById('Tails');
-//         const resultText = document.getElementById("result");
-//         resultText.textContent = coin; // Update text
+    function flip(coin) {
+        const headsImage = document.getElementById('Heads');
+        const tailsImage = document.getElementById('Tails');
+        const resultText = document.getElementById("result");
+        resultText.textContent = coin; // Update text
 
-//         if (coin === "heads") {
-//             headsImage.classList.remove('hidden');
-//             tailsImage.classList.add('hidden');
-//         } else {
-//             headsImage.classList.add('hidden');
-//             tailsImage.classList.remove('hidden');
-//         }
-//     };
-//     coinReset.addEventListener('click', function () {
-//         currentCoinTotal = [0, 0]
-//         document.getElementById("totalHeads").innerText = currentCoinTotal[0];
-//         document.getElementById("totalTails").innerText = currentCoinTotal[1];
-//         const headsImage = document.getElementById('Heads');
-//         const tailsImage = document.getElementById('Tails');
-//         headsImage.classList.add('hidden');
-//         tailsImage.classList.add('hidden');
-//     });
-// });
+        if (coin === "heads") {
+            headsImage.classList.remove('hidden');
+            tailsImage.classList.add('hidden');
+        } else {
+            headsImage.classList.add('hidden');
+            tailsImage.classList.remove('hidden');
+        }
+    };
+    coinReset.addEventListener('click', function () {
+        currentCoinTotal = [0, 0]
+        document.getElementById("totalHeads").innerText = currentCoinTotal[0];
+        document.getElementById("totalTails").innerText = currentCoinTotal[1];
+        const headsImage = document.getElementById('Heads');
+        const tailsImage = document.getElementById('Tails');
+        headsImage.classList.add('hidden');
+        tailsImage.classList.add('hidden');
+    });
+});
